02f5ebb4e0e4d2dd79b991e020928df6106855da
kelin <kelin410@163.com>
2022-08-12 10:43:22 +0800

    新增Search组件。

---
+++ UIKit/src/main/java/com/kelin/uikit/search/SearchablePage.kt
@@ -0,0 +1,31 @@
+package com.kelin.uikit.search
+
+
+/**
+ * 描述 描述有搜索功能的页面。
+ * 创建人 kelin
+ * 创建时间 2017/2/16  下午6:48
+ * 版本 v 1.0.0
+ */
+interface SearchablePage {
+    /**
+     * 初始化的对象。该方法的调用时机是对象被new出来以后，onCreate之前。
+     */
+    fun onInitSearchPage(searcher: Searcher)
+
+    /**
+     * 获取搜索框内显示的提示文字。
+     */
+    val searchHint: String
+
+    /**
+     * 当需要搜索的时候调用。
+     * @param searchKey 要搜索的关键字。
+     */
+    fun onSearch(searchKey: String)
+
+    /**
+     * 当取消搜索的按钮被点击后调用。
+     */
+    fun onSearchCancel()
+}
---
+++ UIKit/src/main/java/com/kelin/uikit/search/Searcher.kt
@@ -0,0 +1,22 @@
+package com.kelin.uikit.search
+
+/**
+ * **描述: ** 搜索者。
+ *
+ * **创建人: ** kelin
+ *
+ * **创建时间: ** 2018/8/15  下午1:56
+ *
+ * **版本: ** v 1.0.0
+ */
+interface Searcher {
+    /**
+     * 搜索
+     */
+    fun search(searchKey: String?)
+
+    /**
+     * 现在搜索历史页面。
+     */
+    fun showHistory()
+}
