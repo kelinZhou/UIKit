1ec807e55e8e54478210b06fff59a01299cdfe7d
kelin <kelin410@163.com>
2023-08-22 11:05:31 +0800

    修复颜色值判断不正确的问题

--- UIKit/src/main/java/com/kelin/uikit/common/Option.kt
+++ UIKit/src/main/java/com/kelin/uikit/common/Option.kt
@@ -10,5 +10,11 @@ package com.kelin.uikit.common
  * **版本:** v 1.0.0
  */
 interface Option : IOption {
+    /**
+     * 为新的页面设置标题是否居中显示，仅在没有调用immersion或immersionToolbar方法时生效。
+     * @param center 标题是否居中显示，没有设置时默认为居中显示。
+     * @see immersion
+     * @see immersionToolbar
+     */
     fun titleCenter(center: Boolean)
 }
